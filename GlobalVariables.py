40ce0d82e90f911b22b0e65b39165ea8fa8b75b0
Jianger <ethantianshujiang@gmail.com>
2022-06-02 23:32:17 -0600

    added preliminary random forest modeling

---
+++ GlobalVariables.py
@@ -0,0 +1,5 @@
+# indexes for data - indicates variables to train on and predict on
+
+PREDICT_INDEX = 1
+START_TRAINING_INDEX = 2
+END_TRAINING_INDEX = 11
---
+++ RandomForest.py
@@ -0,0 +1,38 @@
+import h2o  # do we need the entire package?
+from h2o.estimators import H2ORandomForestEstimator
+import numpy as np
+import GlobalVariables
+
+
+def getRandomForest(data):
+    """
+    :param data:
+    :return forest:
+    """
+    h2o.init()
+    print("Starting Random Forest...")
+    # Create a random forest classifier
+    forest = H2ORandomForestEstimator(ntrees=100)
+
+    # create factors
+    # labels = ["patch", "outcome",
+    #           "Team1Champ1", "Team1Champ2", "Team1Champ3", "Team1Champ4", "Team1Champ5",
+    #           "Team2Champ1", "Team2Champ2", "Team2Champ3", "Team2Champ4", "Team2Champ5"]
+    # dataCopy = np.vstack([labels, data])
+    dataCopy = h2o.upload_file("champions_queue_data.csv")
+    dataCopy["outcome"] = dataCopy["outcome"].asfactor()
+    predictors = ["Team1Champ1", "Team1Champ2", "Team1Champ3", "Team1Champ4", "Team1Champ5",
+                  "Team2Champ1", "Team2Champ2", "Team2Champ3", "Team2Champ4", "Team2Champ5"]
+    response = "outcome"
+
+    # Split data into training and test sets
+    train, valid = dataCopy.split_frame(ratios=[.8], seed=69420)
+
+    # Train the model on the training data
+    forest.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+
+    # Evaluate on the test data
+    perf = forest.model_performance()
+    print(perf)
+    # Return the random forest
+    return forest
